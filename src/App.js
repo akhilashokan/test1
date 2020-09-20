0db904f689ce9920f6e66bb71c051fb69f84535f
Akhil Ashokan <akhilashokan1996@gmail.com>
2020-09-20 14:09:14 +0530

    same but with hook

--- src/App.js
+++ src/App.js
@@ -1,53 +1,43 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
-class Listform extends React.Component{
-  
-  constructor(){
-    super()
-    this.state={
-      placeHolder:'Enter any text',
-      value:'',
-      list:[],  
-    }
-    this.handleChange=this.handleChange.bind(this); 
-    this.handleSubmit=this.handleSubmit.bind(this);  
+
+function Test(){
+  const [item,setItem]=useState('');
+  const [list,setList]=useState([]);
+  var itemList=[];
+  itemList=list.map((t)=><li key={t.name} >{t}</li>);
+
+  function handleChange(e){
+    setItem(e.target.value);
   }
-
-  handleChange(e){
-    this.setState({value:e.target.value})
-  }
-
-
-  handleSubmit(e){
-    const tempList=this.state.list.concat();
-    tempList.push(this.state.value);
-    this.setState({list:tempList});
-    //alert(this.state.list);
+  function handleSubmit(e) {
+    setList(list=>[...list,item]);
+    setItem('');
+    console.log(list);
+    console.log(itemList);
     e.preventDefault();
-    }
 
-
-    render(){
-      const listitems=this.state.list.map((t)=> <li key={t.name} >{t}</li> )
-      return(
-        <div class="box">
-            <form  onSubmit={this.handleSubmit} class='form'>
-                <input type="text" placeholder={this.state.placeHolder}  onChange={this.handleChange}/>
-                <input type="submit" value ="submit"/>
-            </form>
-            <h1> {listitems}</h1>
-        </div>
-      )
-    }
-
-
+  }
+  
+  return(
+    <div>
+        <form onSubmit={handleSubmit}>
+        <input type='text' onChange={handleChange} value={item}/> 
+        <input type='submit' value='submit'/>
+        </form>
+            <ul> {itemList} </ul>
+    </div>
+  )
 }
 
+
+
 function App() {
+
   return (
     <div className="App">
-      <Listform/>
+      <Test/>
     </div>
   );
 }
